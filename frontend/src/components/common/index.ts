fe86c3bc808bed76606fb106b03e43379b762fe4
조성규 <whtjdrb020@gmail.com>
2026-01-07 16:18:15 +0900

    feat(frontend): common 컴포넌트 통합 export 파일

---
+++ frontend/src/components/common/index.ts
@@ -0,0 +1,5 @@
+// Common Components
+export { Button, type ButtonProps } from './Button';
+export { Input, type InputProps } from './Input';
+export { Loading, type LoadingProps } from './Loading';
+export { Icon, type IconProps, type IconName } from './Icon';
