d4b1568d207d1c32cdbcaff97cb5e7667faf0ac1
조성규 <whtjdrb020@gmail.com>
2026-01-07 19:47:46 +0900

    feat: 스켈레톤 로딩 UI 추가
    - components/common/Skeleton 컴포넌트 추가
    - 기본 Skeleton: width, height, variant 지원
    - PlaceItemSkeleton, PlaceListSkeleton: 검색 결과용
    - FavoriteItemSkeleton, FavoriteListSkeleton: 즐겨찾기용
    - HistoryItemSkeleton, HistoryListSkeleton: 히스토리용
    - DetailSkeleton: 상세 패널용
    - 시머 애니메이션 효과

--- frontend/src/components/common/index.ts
+++ frontend/src/components/common/index.ts
@@ -4,4 +4,14 @@ export { Input, type InputProps } from './Input';
 export { Loading, type LoadingProps } from './Loading';
 export { Icon, type IconProps, type IconName } from './Icon';
 export { ToastContainer } from './Toast';
-
+export {
+  Skeleton,
+  PlaceItemSkeleton,
+  PlaceListSkeleton,
+  FavoriteItemSkeleton,
+  FavoriteListSkeleton,
+  HistoryItemSkeleton,
+  HistoryListSkeleton,
+  DetailSkeleton,
+  type SkeletonProps,
+} from './Skeleton';
