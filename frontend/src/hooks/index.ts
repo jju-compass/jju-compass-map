19d627fa32c22ce334b5cab614175c3382bc5827
조성규 <whtjdrb020@gmail.com>
2026-01-13 12:03:02 +0900

    refactor: 사용되지 않는 Hooks 제거 (2단계)
    - useScrollAnimation 삭제
    - useClusterer 삭제
    - useMarkerAnimation 삭제

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -2,8 +2,6 @@ export { useSearch } from './useSearch';
 export { useFavorites } from './useFavorites';
 export { useGeolocation } from './useGeolocation';
 export { useHistory } from './useHistory';
-export { useClusterer } from './useClusterer';
-export { useMarkerAnimation } from './useMarkerAnimation';
 export { useWalkingAnimation } from './useWalkingAnimation';
 export {
   useKeyboardShortcuts,
