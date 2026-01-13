c3b88201ba62a6902a090b9d974cb2043525f519
조성규 <whtjdrb020@gmail.com>
2026-01-13 13:16:56 +0900

    refactor: 미사용 Common 컴포넌트 제거 (2단계)
    - Input 컴포넌트 폴더 전체 삭제 (import 없음)
    - Skeleton 컴포넌트 폴더 전체 삭제 (외부 사용 없음)
    - common/index.ts에서 관련 export 제거

--- frontend/src/components/common/index.ts
+++ frontend/src/components/common/index.ts
@@ -1,19 +1,7 @@
 // Common Components
 export { Button, type ButtonProps } from './Button';
-export { Input, type InputProps } from './Input';
 export { Loading, type LoadingProps } from './Loading';
 export { Icon, type IconProps, type IconName } from './Icon';
 export { ToastContainer } from './Toast';
-export {
-  Skeleton,
-  PlaceItemSkeleton,
-  PlaceListSkeleton,
-  FavoriteItemSkeleton,
-  FavoriteListSkeleton,
-  HistoryItemSkeleton,
-  HistoryListSkeleton,
-  DetailSkeleton,
-  type SkeletonProps,
-} from './Skeleton';
 export { SoundToggle } from './SoundToggle';
 export { SkipLink } from './SkipLink';
